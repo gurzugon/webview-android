f08610b850ed652b6beca9e72d56a47aabcd3c3d
Syahrin Suhaimi <60390413+gurzugon@users.noreply.github.com>
2020-12-30 19:05:28 +0800

    Update SplashActivity.java
    Changing layout for splash screen.

--- app/src/main/java/com/example/eyed/SplashActivity.java
+++ app/src/main/java/com/example/eyed/SplashActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends Activity {
-
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
